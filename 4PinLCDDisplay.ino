4621ff3c55048783c2741586f0daad7fe1b86beb
Atoms Lab <43450165+atomslabs@users.noreply.github.com>
2019-01-12 11:15:22 +0400

    Initial Commit
    This sample code utilize the 16*2 4 PIN LCD

---
+++ 4PinLCDDisplay.ino
@@ -0,0 +1,20 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+ Serial.begin(9600);
+ lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+}
+void loop() {
+     lcd.setCursor(0, 1);
+     lcd.print(":) 4ATOMS.COM (:");
+    
+  delay(3000);
+  lcd.clear();
+}
+
